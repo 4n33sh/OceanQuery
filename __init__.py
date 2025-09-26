4d4cde327b374416acfa1113866114194d2e10bc
Aneesh Sai Praneeth <aneesh.sp222@gmail.com>
2025-09-27 06:56:00 +1100

    CORE: calibration + HFT's netCDF prser
    added: (main) calibration metrics TUNED for the MIT raw ARGO data
    extras: (isolated) config + json workspaces

--- __init__.py
+++ __init__.py
@@ -1,5 +1,29 @@
-# -*- coding: utf-8 -*-
+"""Global import controler"""
 
-__author__ = "Samapriya Roy"
-__email__ = "samapriya.roy@gmail.com"
-__version__ = "0.0.7"
+# pylint: disable=broad-except
+try:
+    import pkg_resources
+
+    __version__ = pkg_resources.get_distribution("pyowc").version
+except Exception:
+    # Local copy, not installed with setuptools, or setuptools is not available.
+    # Disable minimum version checks on downstream libraries.
+    __version__ = "999"
+
+from . import calibration, configuration, core, data, helper, plot, utilities
+from .plot import dashboard
+
+#
+__all__ = (
+    # Sub-packages,
+    "configuration",
+    "calibration",
+    "core",
+    "data",
+    "plot",
+    "dashboard",
+    "utilities",
+    # Constants
+    "__version__",
+    "helper",
+)
